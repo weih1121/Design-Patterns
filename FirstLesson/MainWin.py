e43519823e7418b431ef9e4efa20edaa9cabb628
WH <2586898626@qq.com>
2018-03-21 08:06:42 +0800

    小例子 体会设计模式原则

---
+++ FirstLesson/MainWin.py
@@ -0,0 +1,71 @@
+class Point:
+    __pos_x
+    __pos_y
+
+    def __init__(self, x, y):
+        self.__pos_x = x
+        self.__pox_y = y
+
+class Line:
+    __Point_start
+    __Point_end
+
+    def __init__(self, Point1, Point2):
+        self.__Point_start = Point1
+        self.__Point_end   = Point2
+
+class Rect:
+    __Point_left_top
+    __Width
+    __Height
+
+    def __init__(self, Point, width, height):
+        self.__Point_left_top = Point
+        self.__Width          = width
+        self.__Height         = height
+
+class Form:
+    pass; 
+
+class MainForm(Form):
+    __Point1
+    __Point2
+    __LineList
+    __RectList
+    def __init__(self)
+        super.__init__()
+        pass
+    
+    def OnMouseDown(self, e):
+        #追踪鼠标动作
+        self.__Point1.x = e.x
+        self.__Point1.y = e.y
+        #...
+        pass
+
+    def OnMouseUp(self, e):
+        self.__Point2.x = e.x
+        self.__Point2.y = e.y
+        if (e.action() == drawLine)
+            self.drawLine(self.__Point1, self.__Point2)
+        elif (e.action() == drawRect):
+            width = abs(self.__Point1.x - self.__Point2.x)
+            height = abs(self.__Point1.y - self.__Point2.y)
+            self.drawRect(self.__Point1, width, height)
+        else:
+            pass
+        self.update()   #更新窗口操作
+        pass
+
+
+    def OnPaint(self, e):
+        pass
+
+    def drawLine(self, Point_start, Point_end):
+        pass
+
+    def drawRect(self, Point_left_top, width, height):
+        pass
+
+
+
