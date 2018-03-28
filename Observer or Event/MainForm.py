5caf0f66bc5d397b4357a653868272256505555a
weih1121 <2586898626@qq.com>
2018-03-28 21:53:32 +0800

    观察者模式

---
+++ Observer or Event/MainForm.py
@@ -0,0 +1,13 @@
+class Form:
+    pass
+
+class MainForm(Form):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def split:
+        #1.读取大文件
+
+        #2分批次向小文件写入
+          
